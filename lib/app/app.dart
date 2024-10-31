eb1e7d5a21c5863cf54add26dbec6b9defc1df4a
eryklorenc <reus2000@wp.pl>
2024-10-31 12:54:15 +0100

    Add intl

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:video_task/app/core/theme/app_colors.dart';
 import 'package:video_task/app/core/theme/app_text_theme_extension.dart';
 import 'package:video_task/features/home/home_page.dart';
+import 'package:video_task/generated/l10n.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      supportedLocales: S.delegate.supportedLocales,
+      localizationsDelegates: const [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: AppColors.deepPurple),
         useMaterial3: true,
--- lib/features/home/home_page.dart
+++ lib/features/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:video_task/app/core/theme/app_text_theme_extension.dart';
+import 'package:video_task/generated/l10n.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -9,7 +10,7 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       body: Center(
         child: Text(
-          'Start',
+          S.of(context).start,
           style: Theme.of(context).xTextTheme.display1,
         ),
       ),
